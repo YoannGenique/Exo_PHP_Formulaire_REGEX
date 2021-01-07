f30f86ba573812b75b57713d4b13131d2e02cc60
Yoann Génique <genique.yoann@outlook.com>
2021-01-07 23:34:31 +0100

    Maj

--- index.php
+++ index.php
@@ -1,90 +1,89 @@
 <?php
+    // try and catch pour la connexion bdd
     try{
         // Connexion base de donnée
-        $db = new PDO('mysql:host=localhost;dbname=formulaire', 'root','');
-        $db->exec('SET NAMES "UTF8"');
+        $pdo = new PDO('mysql:host=localhost;dbname=formulaire', 'root','');
     
     }catch (PDOException $e){
         echo 'Erreur : '. $e->getMessage();
         die();
     }
-    if($_POST && $_FILES){
-
-        //Vérifie Si les champs ne sont pas vide
-        if(isset($_POST['user_name']) && !empty($_POST['user_name'])
-        && isset($_POST['user_lastname']) && !empty($_POST['user_lastname'])
-        && isset($_POST['user_mail']) && !empty($_POST['user_mail'])
-        && isset($_FILES['user_avatar']) && !empty($_FILES['user_avatar'])
-        && isset($_POST['user_pass']) && !empty($_POST['user_pass'])){
+    // Si une valeur est envoyé et n'est pas vide
+    if(isset($_POST) && isset($_FILES) && !empty($_POST) && !empty($_FILES)){
 
             // On nettoie les données envoyées
             // Supprime les balises HTML et PHP d'une chaîne
             $user_name = strip_tags($_POST['user_name']);
             $user_lastname = strip_tags($_POST['user_lastname']);
             $user_mail = strip_tags($_POST['user_mail']);
-            $user_avatar = strip_tags($_FILES['user_avatar']);
+            $user_avatar = ($_FILES['user_avatar']);
+            // Cripte le mdp en sha1
             $user_pass = sha1(strip_tags($_POST['user_pass']));
-    
-            $p = "/^[a-zA-Z-' ]*$/";
 
-            // if(preg_match($p, $_POST['user_name'])){
-            //     $pre="correct";
-            // }else{
-            //     $pre="incorrect";
-            // }
-
-            // if(preg_match($p, $_POST['user_lastname'])){
-            //     $las="correct";
-            // }else{
-            //     $las="incorrect";
-            // }
-
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $mailErr = "Invalid email";
-            }
-
+            // definis le valeur à maxsize(poid), ...
             define('MAX_SIZE', 600000);
             define('WIDTH_MAX', 1000);
             define('HEIGHT_MAX', 1000);
-             
-                $file_name = $_FILES['user_avatar']['name'];
-            
-                $file_extension = strrchr($file_name,".");
-           
-                $file_tmp_name = $_FILES['user_avatar']['tmp_name'];
-                
-                $file_dest= 'img/'.$file_name;
-        
-                $extension_autorise = array('.jpeg','.jpg','.png','.gif','.webp');
-        
-                if (in_array($file_extension, $extension_autorise)){
 
-                    if((filesize($_FILES['user_avatar']['tmp_name']) <= MAX_SIZE)){
-        
-                        if(move_uploaded_file($file_tmp_name, $file_dest)){
-        
-                            echo 'Fichier envoyé';
-        
+            // IMG 
+            // nom de l'img
+            $file_name = $_FILES['user_avatar']['name'];
+            // on recupère le type de la photo(jpg,png...)
+            $file_extension = strrchr($file_name,".");
+            // img dossier temp
+            $file_tmp_name = $_FILES['user_avatar']['tmp_name'];
+            // Chemin du dossier de sav
+            $file_dest= 'img/'.$file_name;
+            // extension de photo autorisée
+            $extension_autorise = array('.jpeg','.jpg','.png','.gif','.webp');
+            // Fin IMG
+
+            // Regex
+            // Rentre les valeurs de regex dans une var qui sera use plus tard 
+            $regname = "/^[a-zA-Z-' ]*$/";
+            // Fin Regex
+
+            // Si la valeur rentrer par l'user ne contient que des Lettre en Maj ou Min de A-Z ON PASSE AU AUTRE VERIF
+            if(preg_match($regname, $user_name)){
+
+                if(preg_match($regname, $user_lastname)){
+                    // Se renseigner sur les filter var et surtt sur FILTER_VALIDATE_EMAIL
+                    if (filter_var($user_mail, FILTER_VALIDATE_EMAIL)){
+                        // Import Img 
+                        if (in_array($file_extension, $extension_autorise)){
+
+                            if((filesize($_FILES['user_avatar']['tmp_name']) <= MAX_SIZE)){
+                
+                                if(move_uploaded_file($file_tmp_name, $file_dest)){
+                
+                                    //après verification du nom prenom mail on execute la requete
+                                    $result=$pdo->prepare("INSERT INTO user(user_name, user_lastname, user_mail, user_avatar, user_pass) VALUES (?,?,?,?,?)");
+                                    $result->execute(array($user_name, $user_lastname, $user_mail, $file_dest, $user_pass));
+
+                                    echo "User enregistrer";
+                                    // On fait la redirection
+                                    header('Location:profil.php');
+                
+                                }else{
+                                    echo "Une erreur lors de l'envoi du fichier";
+                                }
+                            }else{
+                                echo "Image Trop Lourd";
+                            }
                         }else{
-                            echo"Une erreur lors de l'envoi du fichier";
+                            echo "Extensions non autorisée, extension autorisée (jpeg, jpg, png, gif, webp)";
                         }
+
                     }else{
-                        echo 'Trop Lourd';
+                        echo "Email Invalid";
                     }
                 }else{
-                    echo 'Extensions non autorisée';
+                    echo "Veuillez écrire votre Nom de Famille sans caractère spéciaux";
                 }
-                   
-                // Insert dans la Table formulaire, les champs remplie correspondant à la ligne correspondant
-                $sql = 'INSERT INTO `user` (`user_name`, `user_lastname`, `user_mail`, `user_avatar`, `user_pass`) VALUES (:user_name, :user_lastname, :user_mail, :file_dest, :user_pass)';
-                // On prepare la requête
-                $query = $db->prepare($sql);
-                // excute tt les param rentrer
-                $query->execute();
-                // On fait la redirection vers la dashboard ou sera affiché le message 
-                header('Location:profil.php');
+            }else{
+                echo "Veuillez écrire votre Prénom sans caractère spéciaux";
+            }
     }
-}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -102,50 +101,20 @@
         <section>
             <div class="form">
                 <div class="titre">
-                    <h1>Inscription</h1>               
+                    <h1>Inscription</h1>  
                 </div>
                 <form method="POST" enctype="multipart/form-data">
                     <div class="mb-3">
                         <label for="user_name" class="form-label">Nom Utilisateur</label>
                         <input name="user_name" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <?php 
-                        $p = "/^[a-zA-Z-' ]*$/";
-
-                        if(isset($_POST['user_name'])){
-                            if(preg_match($p, $_POST['user_name'])){
-                                $pre="correct";
-                            }else{
-                                header('index.php');
-                                $pre="incorrect";
-                            }
-                        }         
-                        ?>
                     </div>
                     <div class="mb-3">
                         <label for="user_lastname" class="form-label">Nom de Famille Utilisateur</label>
                         <input name="user_lastname" type="text" class="form-control" id="exampleInputPassword1">
-                        <?php
-                        if(isset($_POST['user_lastname'])){
-                            if(preg_match($p, $_POST['user_lastname'])){
-                                $las="correct";
-                            }else{
-                                header('index.php');
-                                $las="incorrect";
-                            }
-                        }
-                        ?>
                     </div>
                     <div class="mb-3">
                         <label for="user_mail" class="form-label">Email Utilisateur</label>
                         <input name="user_mail" type="email" class="form-control" id="exampleInputPassword1">
-                        <?php
-                        if(isset($_POST['user_mail'])){
-                            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                                header('index.php');
-                                $mailErr = "Invalid email";
-                            }
-                        }
-                        ?>
                     </div>
                     <div class="mb-3">
                         <label for="user_avatar" class="form-label">Photo de Profil Utilisateur</label>
